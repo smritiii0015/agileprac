6335e60ef2d11dc2c6278f79c5ca34a54eda2bcb
smritinaik <148971067+smritinaik@users.noreply.github.com>
2026-04-01 19:56:49 +0530

    java file push

---
+++ new.java
@@ -0,0 +1,7 @@
+public class new {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a print statement");
+        System.out.println("Welcome to Java!");
+    }
+}
